a3c592c0a782f9877baff1175c5312b0eecd0709
Karthik Tiruvarur <tiruvarur.karthik@gmail.com>
2026-07-02 12:30:13 -0500

    update

--- push-site.bat
+++ push-site.bat
@@ -5,7 +5,7 @@ REM  One-click commit + push for Karthik's website repo
 REM  Repo: https://github.com/tgrkarthik/tgrkarthik.github.io
 REM ============================================================
 
-cd /d "C:\Users\tgrka\Karthik WEBSITE\tgrkarthik.github.io-main"
+cd /d "C:\Users\tgrka\CLAUDE GLOBAL FOLDER\KARTHIK WEBSITE\tgrkarthik.github.io-main"
 
 echo.
 echo === Current status ===
